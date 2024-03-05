7737c138fd6aa434f5996e710962242aa631885e
rice_cooker <89035491+11255544@users.noreply.github.com>
2024-03-05 12:08:37 +0100

    Update create.sql

--- sql/úkoly/26.2.2023/create.sql
+++ sql/úkoly/26.2.2023/create.sql
@@ -1,1 +1,24 @@
 
+-- Vytvoření tabulky stravnici
+CREATE TABLE stravnici (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    jmeno VARCHAR(50) NOT NULL,
+    platba BOOLEAN DEFAULT false
+);
+
+-- Vytvoření tabulky jidla
+CREATE TABLE jidla (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nazev VARCHAR(100) NOT NULL,
+    cena DECIMAL(10, 2) NOT NULL
+);
+
+-- Vytvoření tabulky objednavky
+CREATE TABLE objednavky (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    stravnik_id INT,
+    jidlo_id INT,
+    datum DATE,
+    FOREIGN KEY (stravnik_id) REFERENCES stravnici(id),
+    FOREIGN KEY (jidlo_id) REFERENCES jidla(id)
+);
